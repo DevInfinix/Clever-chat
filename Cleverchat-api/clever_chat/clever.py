4995ba79340c621168aa51477037bb204640c72b
INFINIX <72700816+DevInfinix@users.noreply.github.com>
2022-03-10 22:34:18 +0530

    Update clever.py

--- Cleverchat-api/clever_chat/clever.py
+++ Cleverchat-api/clever_chat/clever.py
@@ -3,17 +3,17 @@ import requests
 import json
 
 class AsyncClient():
-    async def get_response(message: str, user = None, gender: str = "Male", chatbot_name: str = "Clever Bot", developer_name: str = "Pr0methium", age: str = "8", birthday: str = "March 18", birthplace: str = "acobot.ai", birthyear: str = "2012", religion: str = "Christian", favouriteactor: str = "Tom Hanks", favouriteactress: str = "Julia Roberts", favouriteartist: str = "Leonardo da Vinci", favouriteauthor: str = "Ernest Hemingway", favouriteband: str = "Beatles", favouritebook: str = "The Odyssey"):
+    async def get_response(message: str, user = None, gender: str = "Male", chatbot_name: str = "ABLETON", developer_name: str = "`INFINIX#7276`", age: str = "3", birthday: str = "October 5", birthplace: str = "Mumbai, India", birthyear: str = "2022", religion: str = "ABLE", favouriteactor: str = "Tom Hanks", favouriteactress: str = "Julia Roberts", favouriteartist: str = "Leonardo da Vinci", favouriteauthor: str = "Ernest Hemingway", favouriteband: str = "Beatles", favouritebook: str = "The Odyssey"):
         async with aiohttp.ClientSession() as session:
-            request = await session.get(f"https://noideawhatthisis.hisroyal123.repl.co/?message={message}&user={user}&gender={gender}&name={chatbot_name}&developer_name={developer_name}&age={age}&birthday={birthday}&birthplace={birthplace}&birthyear={birthyear}&religion={religion}&favouriteactor={favouriteactor}&favouriteactress={favouriteactress}&favouriteartist={favouriteartist}&favouriteauthor={favouriteauthor}&favouriteband={favouriteband}&favouritebook={favouritebook}")
+            request = await session.get(f"https://yourmommmaosamaobama.hisroyal123.repl.co/?message={message}&user={user}&gender={gender}&name={chatbot_name}&developer_name={developer_name}&age={age}&birthday={birthday}&birthplace={birthplace}&birthyear={birthyear}&religion={religion}&favouriteactor={favouriteactor}&favouriteactress={favouriteactress}&favouriteartist={favouriteartist}&favouriteauthor={favouriteauthor}&favouriteband={favouriteband}&favouritebook={favouritebook}")
             fetch = await request.json()
             response = fetch["message"]
             await session.close()
             return response
 
 class Client():
-    def get_response(message: str, user = None, gender: str = "Male", chatbot_name: str = "Clever Bot", developer_name: str = "Pr0methium", age: str = "8", birthday: str = "March 18", birthplace: str = "acobot.ai", birthyear: str = "2012", religion: str = "Christian", favouriteactor: str = "Tom Hanks", favouriteactress: str = "Julia Roberts", favouriteartist: str = "Leonardo da Vinci", favouriteauthor: str = "Ernest Hemingway", favouriteband: str = "Beatles", favouritebook: str = "The Odyssey"):
-        request = requests.get(f"https://noideawhatthisis.hisroyal123.repl.co/?message={message}&user={user}&gender={gender}&name={chatbot_name}&developer_name={developer_name}&age={age}&birthday={birthday}&birthplace={birthplace}&birthyear={birthyear}&religion={religion}&favouriteactor={favouriteactor}&favouriteactress={favouriteactress}&favouriteartist={favouriteartist}&favouriteauthor={favouriteauthor}&favouriteband={favouriteband}&favouritebook={favouritebook}")
+    def get_response(message: str, user = None, gender: str = "Male", chatbot_name: str = "ABLETON", developer_name: str = "`INFINIX#7276`", age: str = "3", birthday: str = "October 5", birthplace: str = "Mumbai, India", birthyear: str = "2022", religion: str = "ABLE", favouriteactor: str = "Tom Hanks", favouriteactress: str = "Julia Roberts", favouriteartist: str = "Leonardo da Vinci", favouriteauthor: str = "Ernest Hemingway", favouriteband: str = "Beatles", favouritebook: str = "The Odyssey"):
+        request = requests.get(f"https://yourmommmaosamaobama.hisroyal123.repl.co/?message={message}&user={user}&gender={gender}&name={chatbot_name}&developer_name={developer_name}&age={age}&birthday={birthday}&birthplace={birthplace}&birthyear={birthyear}&religion={religion}&favouriteactor={favouriteactor}&favouriteactress={favouriteactress}&favouriteartist={favouriteartist}&favouriteauthor={favouriteauthor}&favouriteband={favouriteband}&favouritebook={favouritebook}")
         fetch = request.json()
         response = fetch["message"]
         return response
